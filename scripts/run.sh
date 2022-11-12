b7739c0c26834656136f2a741ddb675c4546f812
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-11-11 16:18:50 -0800

    plot

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,13 @@
-python ./main.py --strategy=only_spot \
+for i in `seq 52 4 92`; do
+python ./main.py --strategy=pair_amortize \
+                --pair-interval-hours=8 \
+                --use-avg-gain \
                 --env trace \
                 --restart-overhead-hours=0.1 \
                 --trace-file data/real/ping_based/us-west-2a_v100_1.txt \
-                --deadline-hours=1000 \
+                --deadline-hours=$i \
                 --task-duration-hours=48
-
+done
 
 
                 # --pair-interval-hours=8 \
