f4105a6312db70755675b944188437cafe02d378
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-12-16 12:45:19 -0800

    simulate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import configargparse
 import copy
+import json
+import os
 
 import numpy as np
 import tqdm
@@ -55,7 +57,7 @@ def main():
     spot_costs = []
     cost_ratio = []
 
-    trace_file = args.trace_file.split('/')[-1].split('.')[0]
+    trace_file = args.trace_file.split('/')[-2].split('.')[0]
     env_name = envs[0].NAME
     env_config = envs[0].config
     run_name = f'{strategy.name}-{env_name}-{trace_file}-ddl={args.deadline_hours}-dur={args.task_duration_hours}-over={args.restart_overhead_hours}'
@@ -78,26 +80,35 @@ def main():
 
         costs.append(simulate(env, strategy))
     
-        if len(envs) > 1:
-            env.reset()
-            new_args = copy.deepcopy(args)
-            new_args.deadline_hours = 1000
-            spot_strategy = sky_spot.strategies.only_spot.OnlySpotStrategy(new_args)
-            spot_strategy.register_env(env)
-            spot_costs.append(simulate(env, spot_strategy))
-            cost_ratio.append(costs[-1] / spot_costs[-1])
+        # if len(envs) > 1:
+        #     env.reset()
+        #     new_args = copy.deepcopy(args)
+        #     new_args.deadline_hours = 1000
+        #     spot_strategy = sky_spot.strategies.only_spot.OnlySpotStrategy(new_args)
+        #     spot_strategy.register_env(env)
+        #     spot_costs.append(simulate(env, spot_strategy))
+        #     cost_ratio.append(costs[-1] / spot_costs[-1])
+        
         
 
-        mean_strategy_cost = np.mean(costs)
-        std_strategy_cost = np.std(costs)
-        mean_spot_cost = np.mean(spot_costs)
-        std_spot_cost = np.std(spot_costs)
-        mean_cost_ratio = np.mean(cost_ratio)
-        std_cost_ratio = np.std(cost_ratio)
-        msg = f'cost: {mean_strategy_cost:.2f}±{std_strategy_cost:.2f}; spot cost: {mean_spot_cost:.2f}±{std_spot_cost:.2f}; cost ratio: {mean_cost_ratio:.2f}±{std_cost_ratio:.2f}'
-        print('=== ' + msg + ' ===')
-        pbar.set_description(msg)
-        wandb.log({'MeanCost': mean_strategy_cost, 'StdCost': std_strategy_cost, 'MeanSpotCost': mean_spot_cost, 'StdSpotCost': std_spot_cost, 'MeanCostRatio': mean_cost_ratio, 'StdCostRatio': std_cost_ratio})
+        # mean_strategy_cost = np.mean(costs)
+        # std_strategy_cost = np.std(costs)
+        # mean_spot_cost = np.mean(spot_costs)
+        # std_spot_cost = np.std(spot_costs)
+        # mean_cost_ratio = np.mean(cost_ratio)
+        # std_cost_ratio = np.std(cost_ratio)
+        # msg = f'cost: {mean_strategy_cost:.2f}±{std_strategy_cost:.2f}; spot cost: {mean_spot_cost:.2f}±{std_spot_cost:.2f}; cost ratio: {mean_cost_ratio:.2f}±{std_cost_ratio:.2f}'
+        # print('=== ' + msg + ' ===')
+        # pbar.set_description(msg)
+        # wandb.log({'MeanCost': mean_strategy_cost, 'StdCost': std_strategy_cost, 'MeanSpotCost': mean_spot_cost, 'StdSpotCost': std_spot_cost, 'MeanCostRatio': mean_cost_ratio, 'StdCostRatio': std_cost_ratio})
+
+    os.makedirs('exp/', exist_ok=True)
+    with open(f'exp/{run_name}', 'w') as f:
+        json.dump({
+            'args': args,
+            'costs': costs,
+        }, f)
+    print('mean: ', np.mean(costs), '; std: ', np.std(costs), '; worst 1%: ', np.percentile(costs, 99), '; worst 10%: ', np.percentile(costs, 90))
 
 
 if __name__ == '__main__':
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,19 @@
 # for i in 56 64 72; do
 # for i in 66; do
+for rate in 0.1 0.2 0.3 0.4 0.5 0.6; do
 python ./main.py --strategy=strawman \
                 --env trace \
-                --trace-file data/poisson/gap_600-hourly_rate_0.25 \
+                --trace-file data/poisson/gap_600-hourly_rate_$rate \
                 --restart-overhead-hours=0.1 \
                 --deadline-hours=52 \
                 --task-duration-hours=48
+done
 # done
+                # --pair-interval-hours=8 \
+                # --use-avg-gain \
 
                 # --trace-file data/real/ping_based/us-west-2a_v100_1.txt \
                 # --env-start-hours=72 \
-                # --pair-interval-hours=8 \
-                # --use-avg-gain \
 
                 # --restart-overhead-hours=0.5 \
                 # --trace-file data/poisson/gap_1200-hourly_rate_0.1/1.json \
